b05815b6163c7aa0f8f103fb16a40c5d6f8a5517
Rodrigo Gutierrez Martinez <rodrigogutierrezmtz@gmail.com>
2026-09-09 23:05:24 -0600

    Primer ejercicio

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,1 +1,9 @@
-print("Hola mundo")
+print("Dame tu nombre")
+nombre = input()
+print(f"Hola, {nombre}!")
+print("Dame tu edad")
+edad = int(input())
+
+año = 2026 + (100 - edad)
+
+print(f"{nombre} cumplirá 100 años en {año}")
